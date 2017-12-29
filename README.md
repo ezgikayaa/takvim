0d1c2be166ae6758411f67236ec5d5903f7ee861
ezgikayaa <ezgikayaa24@gmail.com>
2017-12-29 23:22:49 +0300

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sample
-Kocaeli university open source lab yii2 sample module
+# Takvim
+Kocaeli university open source lab yii2 Takvim module
+
+http://kouosl-app.dev/takvim/  Frontend modül sayfası.
+http://kouosl-app.dev/admin/takvim/  Backend modül sayfası.
+
+http://kouosl-app.dev/admin/takvim/takvims/ Projemin Backend kısmında beklenilen "Ekleme" , "Silme" , "Güncelleme" , "Gösterme" işlemlerini yapmaktadır.
+http://kouosl-app.dev/admin/takvim/tatils/  Projemin Backend kısmında beklenilen "Ekleme" , "Silme" , "Güncelleme" , "Gösterme" işlemlerini yapmaktadır. 
+
+
+http://kouosl-app.dev/takvim/takvims/ Günün menüsü ekranda gösterilmektedir.
+
+Projede migration ayarlarını yapamadığımdan phpmyadmindeki "takvim-modulu" isimli veritabanımı kendi veritabanınıza eklemeniz gerekiyor.
+Database klasörünün altında database mevcuttur.
--- views/frontend/Takvims/index.php
+++ views/frontend/Takvims/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel kouosl\takvim\models\TakvimSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Takvims';
+$this->title = 'Takvim';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="takvim-index">
@@ -15,23 +15,21 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Takvim', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            
 
             'takvim_date',
             'gun',
             'ay',
             'yil',
             'saat',
-            // 'takvimtip',
+             'takvimtip',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            
         ],
     ]); ?>
 </div>
